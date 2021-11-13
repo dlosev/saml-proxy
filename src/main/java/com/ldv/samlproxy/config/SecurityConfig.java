10dba902ecd86f82b83409d34372562d49a8763c
Dmitry Losev <dmitrii@powerodit.com>
2021-11-13 20:57:38 +0700

    Deployment workflow

---
+++ src/main/java/com/ldv/samlproxy/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.ldv.samlproxy.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.saml2.provider.service.metadata.OpenSamlMetadataResolver;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
+import org.springframework.security.saml2.provider.service.servlet.filter.Saml2WebSsoAuthenticationFilter;
+import org.springframework.security.saml2.provider.service.web.DefaultRelyingPartyRegistrationResolver;
+import org.springframework.security.saml2.provider.service.web.Saml2MetadataFilter;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * .
+ *
+ * @author Dmitry Losev ldv42@yandex.ru
+ * @since 11/13/21
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public SecurityConfig() {
+        super(true);
+    }
+
+    @Autowired
+    private RelyingPartyRegistrationRepository relyingPartyRegistrationRepository;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.exceptionHandling();
+        http.sessionManagement();
+        http.securityContext();
+        http.anonymous();
+
+        http
+                .authorizeRequests()
+                .antMatchers("/health").permitAll()
+                .anyRequest().authenticated().and()
+                .saml2Login();
+
+        Converter<HttpServletRequest, RelyingPartyRegistration> relyingPartyRegistrationResolver =
+                new DefaultRelyingPartyRegistrationResolver(relyingPartyRegistrationRepository);
+        Saml2MetadataFilter filter = new Saml2MetadataFilter(relyingPartyRegistrationResolver, new OpenSamlMetadataResolver());
+
+        http.addFilterBefore(filter, Saml2WebSsoAuthenticationFilter.class);
+    }
+}
