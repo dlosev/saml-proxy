198b953c1c3cc076078853a48b8f61cdaf3a5b08
Dmitry Losev <dmitrii@powerodit.com>
2021-11-14 17:55:44 +0700

    fix

--- src/main/java/com/ldv/samlproxy/config/SecurityConfig.java
+++ src/main/java/com/ldv/samlproxy/config/SecurityConfig.java
@@ -1,9 +1,6 @@
 package com.ldv.samlproxy.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -44,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
                 .authorizeRequests()
-                .antMatchers("/health", "/httptrace").permitAll()
+                .antMatchers("/health").permitAll()
                 .anyRequest().authenticated().and()
                 .saml2Login();
 
@@ -53,10 +50,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         Saml2MetadataFilter filter = new Saml2MetadataFilter(relyingPartyRegistrationResolver, new OpenSamlMetadataResolver());
 
         http.addFilterBefore(filter, Saml2WebSsoAuthenticationFilter.class);
-    }
-
-    @Bean
-    public HttpTraceRepository httpTraceRepository() {
-        return new InMemoryHttpTraceRepository();
     }
 }
