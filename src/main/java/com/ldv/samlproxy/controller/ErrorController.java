feee10d607c81dc92255cef3a8353aaa18108d73
Dmitry Losev <ldv42@yandex.ru>
2021-11-16 23:27:10 +0700

    Templates path fixed

--- src/main/java/com/ldv/samlproxy/controller/ErrorController.java
+++ src/main/java/com/ldv/samlproxy/controller/ErrorController.java
@@ -1,12 +1,8 @@
 package com.ldv.samlproxy.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * .
@@ -14,19 +10,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @author Dmitry Losev ldv42@yandex.ru
  * @since 11/16/21
  */
-@ControllerAdvice
-public class ErrorController {
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ErrorController.class);
-
-    @ExceptionHandler(Throwable.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @RequestMapping("/error")
     public String exception(final Throwable throwable, final Model model) {
-        LOGGER.error("Exception during execution of SpringSecurity application", throwable);
-
         String errorMessage = (throwable != null ? throwable.getMessage() : "Unknown error");
         model.addAttribute("errorMessage", errorMessage);
 
-        return "error";
+        return "/error";
     }
 }
