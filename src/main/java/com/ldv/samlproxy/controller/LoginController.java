e2a4aa6f9e5936f9892e93cd8faf13225884787a
Dmitry Losev <ldv42@yandex.ru>
2021-12-23 12:58:18 +0700

    Current authenticated user is retrieved in controller

--- src/main/java/com/ldv/samlproxy/controller/LoginController.java
+++ src/main/java/com/ldv/samlproxy/controller/LoginController.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,12 +24,13 @@ public class LoginController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
 
-    @Value( "${custom.login-redirect-url}" )
+    @Value("${custom.login-redirect-url}")
     private String loginRedirectUrl;
 
     @GetMapping
-    public RedirectView redirectWithUsingRedirectView() {
-        LOGGER.debug("Redirecting to {} since request is authenticated", loginRedirectUrl);
+    public RedirectView redirectWithUsingRedirectView(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal) {
+        LOGGER.debug("Redirecting to {} since request is authenticated for user {}",
+                loginRedirectUrl, principal.getName());
 
         return new RedirectView(loginRedirectUrl);
     }
